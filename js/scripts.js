4e83b1336809f8c312c00b3dc2dfd5d958140312
kha le & chase reckling <student@epicodus.com>
2015-05-27 16:02:21 -0700

    translate a word into pig latin

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+var pigLatin = function(word) {
+  if (vowelFinder(word)) {
+    return isVowel(word);
+  } else {
+    return "something";
+  }
+};
+
+var vowelFinder = function(word) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var firstLetter = word[0];
+
+  for (var i in vowels) {
+    for (var j in firstLetter) {
+        if (vowels[i] == firstLetter[j]) {
+          return true;
+      }
+    }
+  }
+};
+
+var vowel = function(letter) {
+  if (letter === "a" || letter === "e" || letter === "o" || letter === "i" || letter === "u") {
+    return true;
+  }
+};
+
+var isVowel = function(word) {
+  return(word+"ay");
+};
+
+
+var isConsonant = function(word) {
+  var consonantMove = word.split("");
+  var consonantWord = [];
+
+    while (! vowel(consonantMove[0])) {
+      var letter = consonantMove.shift()
+      consonantMove.push(letter)
+      var answer = consonantMove.join("")+'ay';
+    }
+    return answer;
+
+};
---
+++ specs/specs.js
@@ -0,0 +1,27 @@
+describe('vowelFinder', function() {
+  it('determines if a word starts with a vowel', function() {
+    expect(vowelFinder('apple')).to.equal(true);
+  });
+
+  it('determines if a word starts with a consonate', function() {
+    expect(!vowelFinder("hat")).to.equal(true);
+  });
+});
+
+describe('isVowel', function() {
+  it('adds ay to the end of a word that starts with a vowel', function() {
+    expect(isVowel('apple')).to.equal("appleay");
+  });
+});
+
+describe('pigLatin', function() {
+  it('will call the isVowel function and add ay to the end of a word', function() {
+    expect(pigLatin('apple')).to.equal('appleay');
+  });
+});
+
+describe('isConsonant', function(){
+  it("moves the first set of consonants up to the first vowel, to the end of the word and adds 'ay'", function(){
+    expect(isConsonant('car')).to.equal('arcay');
+  });
+});
